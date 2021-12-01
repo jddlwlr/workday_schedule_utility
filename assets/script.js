82d6ebafe36e4898307222cbdccb7175e4fd3ec2
Jesse Lawler <jedd.lawler@gmail.com>
2021-11-30 21:29:09 -0700

    Updating style, html and adding functionality

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,33 @@ var update = function() {
 }
 setInterval(update, 1000);
 
+
+
+// Setting currentHour as a number
+var currentHour = moment().hour();
+console.log(currentHour);
+
+// Targeting button Elements
+var eightBtn = $('#eightBtn');
+
+//  Targeting text area Elements
+var eightText = $('#eightText');
+
+
+
+// Color Coding Time blocks
+
+function eight(){
+    var hour = 23;
+    
+    if(currentHour > hour){
+        $('#eightText').addClass('past');
+    }
+    else if(currentHour < hour){
+        $(eightText).addClass('future');
+    }
+    else {
+        $(eightText).addClass('present');
+
+    }
+};
